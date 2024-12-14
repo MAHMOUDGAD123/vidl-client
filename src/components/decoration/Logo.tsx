31c15fc40c22aeca3fd033114309f390dbd354e4
Mahmoud_Gad <hooda.gad333@gmail.com>
2024-12-14 22:04:39 +0200

    fix vidl logo for apple

--- src/components/decoration/Logo.tsx
+++ src/components/decoration/Logo.tsx
@@ -1,16 +1,31 @@
-import { Link } from "react-router-dom";
-import FontIcon from "./FontIcon";
-import { faPlay } from "@fortawesome/free-solid-svg-icons";
 import "./logo.css";
+import { Link } from "react-router-dom";
+import { faPlay } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Logo = () => {
+  const isSafari =
+    navigator.userAgent &&
+    /apple/i.test(navigator.userAgent) &&
+    !navigator.userAgent.includes("Chrome") &&
+    !navigator.userAgent.includes("CriOS") && // chrome on ios
+    !navigator.userAgent.includes("FxiOS"); // firefox on ios
+
   return (
-    <Link to="/" id="logo">
-      <div className="logo-icon">
-        <FontIcon icon={faPlay} />
-      </div>
-      <span>IDL</span>
-    </Link>
+    <>
+      {isSafari ? (
+        <Link to="/" className="logo-safari">
+          <span>VIDL</span>
+        </Link>
+      ) : (
+        <Link to="/" className="logo">
+          <div className="logo-icon">
+            <FontAwesomeIcon icon={faPlay} />
+          </div>
+          <span>IDL</span>
+        </Link>
+      )}
+    </>
   );
 };
 
